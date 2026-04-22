319c3cd09cc56b6a6ec016472a8db9348523a675
ali <olimovalijan616@gmail.com>
2026-04-23 01:04:50 +0500

    deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
